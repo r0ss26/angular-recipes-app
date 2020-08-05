8a2693c141e715a60da67c7a910c6fcc31cccc26
Ross Baker <rossbaker32@gmail.com>
2020-08-05 11:27:15 +1000

    Setup initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-list/recipe-item/recipe-detail/recipe-detail.component';
+import { ShoppingList } from './shopping-list/shopping-list.component'
+import { ShoppingListEdit } from './shopping-list/shopping-list-edit/shopping-list-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingList,
+    ShoppingListEdit
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'shopping-list-edit',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListEdit {}
